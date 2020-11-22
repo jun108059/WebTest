f364f590ec2414ca7804395cfc4e87ef9f6f6eeb
YoungJun_Park <youngjun108059@gmail.com>
2020-11-22 23:55:10 +0900

    [Update]✍POST 처리, Session 적용

--- StockPriceQuery/webquerystock.py
+++ StockPriceQuery/webquerystock.py
@@ -2,6 +2,10 @@
 Created on 2018. 8. 21.
 
 @author: cskim
+
+Modified in 2020.11.22.
+
+@author: Young-Jun Park
 '''
 from flask import Flask
 from flask import render_template
@@ -20,9 +24,27 @@ DB = DBHelper()
 
 @app.route("/", methods=['GET','POST'])
 def home():
-    #session.clear()
+        
+    if request.method == 'POST':
+        gubun = request.form.get('gubun')
+        grade = request.form.get('grade')
+        stu_dept = request.form.get('stu_dept')
+        stu_name = request.form.get('stu_name')
+        queryObj = {'gubun':gubun, 'grade': grade, 'stu_dept':stu_dept, 'stu_name':stu_name}
+        print ('queryObj=', queryObj)        
+        session['queryObj'] = queryObj
+        return redirect(url_for('home'))
 
-    return render_template("home.html")
+    home_param = None
+    if session.get('queryObj'):
+        home_param = session.get('queryObj')
+    
+    # 기본 설정이 안되어 있는 전공으로 check
+    if home_param.get('stu_dept') == None:
+        session.clear()
+        return render_template("home.html")    
+
+    return render_template("home.html", home_param=home_param)
 
 @app.route("/listprice", methods=['GET','POST'])
 def listprice():
@@ -47,7 +69,7 @@ def listprice():
         
     if price_list == None:
         price_list=DB.get_n_price(lim=100)
-    #print len(price_list)
+    # print len(price_list)
     return render_template("list_price_tab.html", price_param=price_param, price_list=price_list)
 
 if __name__ == '__main__':
